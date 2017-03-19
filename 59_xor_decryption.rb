aac345d33022ef6d0925d201cbb610c70a0f10ea
paulizleet <paulitogallagher@gmail.com>
2017-03-19 11:34:04 -0400

    did stuff? maybe i can call it progress?

--- 59_xor_decryption.rb
+++ 59_xor_decryption.rb
@@ -2,12 +2,12 @@
 require_relative "cipher"
 
 CIPHER = ciphertext.split(",").map{|c| c.to_i}
-cipher = CIPHER.dup
-zip = cipher.zip(CIPHER.map{|c| c.chr})
-zip.each{|r| p r}
+CAPS = ("A".."Z").to_a
+SMALLS = ("a".."z").to_a
 $probable = 0
 def recurse(chars, letters)
   if chars.length == 3
+    p chars
     if decrypt(chars) == true
       $probable += 1
 
@@ -35,6 +35,11 @@ def decrypt(chars)
     else
       (CIPHER[i]^chars[0])
     end
+
+    if ("a".."z").include?(decrypted_text[-1]) || ("A".."Z").include?(decrypted_text[-1])
+      return 1 if i == 0
+      return i%3
+    end
     i+=1
   end
 
@@ -47,8 +52,6 @@ def check_ints(text)
   !text.any?{|e| ((65 > e || e > 90) || (97 > e || e > 122)) && e != 32}
 end
 def check_regexp(text)
-  #puts text.join
-#s  gets.chomp
 
   match = /[^\w]/.match(text.map{|m| m.chr}.join)
   if match != nil
@@ -59,10 +62,58 @@ def check_regexp(text)
   true
 end
 
+def every_third(letters)
+  valids = {0 => [], 1 => [], 2 => []}
+  3.times do |i|
+    for e in letters
+      a=0
+      tf = true
+      while a < CIPHER.length
+        break if a+i >= CIPHER.length
+        n = (CIPHER[a+i]^e)
+        if SMALLS.include?(n.chr) || CAPS.include?(n.chr)
+          #puts (CIPHER[a+i]^i).chr
+        #  gets.chomp
+          tf = false
+          break
+        end
+        a+=3
+      end
+      valids[i] << e if tf
 
-lowercase_letters = (97..122).to_a
+    end
+  end
+  p valids
+end
+
+def iterate(possibilities)
+  narrower = []
+  dec = 0
+  for first in possibilities[0]
+    dec = 0
+    for second in possibilities[1]
+      dec = 0
+      for third in possibilities[2]
+        p [first, second, third]
+        dec = decrypt([first, second, third])
+        case dec
+        when 0 then break
+        when 1 then break
+        when 2 then next
+        else narrower << [first, second, third]
+        end
+      end
+
+      break if dec == 0
+    end
+  end
+
+end
+lowercase_letters = (0..255)
 
 p lowercase_letters.map{|a| a.chr(Encoding::ASCII_8BIT)}
 
-recurse([], lowercase_letters)
+possibilities = every_third(lowercase_letters)
+narrowed = iterate(possibilities)
+narrowed.each {|r| p r}
 p $probable
