134fef7ffd263f30bacac0e345103a2c56e9dec0
paulizleet <paulitogallagher@gmail.com>
2017-09-19 14:27:31 -0400

    Solved problem.

---
+++ solved/p25_thousand_digits_fibonacci.rb
@@ -0,0 +1,18 @@
+#what is the index of the first term in the fibonacci sequence to have 1000 digits?
+
+require_relative 'euler_utils'
+
+fib = [1]
+a = [0]
+b = [1]
+i=1
+
+while fib.length < 1000 do
+
+  fib = add_array([a, b])
+  b = a
+  a = fib
+  i+=1
+end
+
+p i
