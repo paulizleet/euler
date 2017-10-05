909f5e7b1cb63f64a8810c94b8b369ed29facba9
paulizleet <paulitogallagher@gmail.com>
2017-10-05 13:17:27 -0400

    Refactored

--- solved/p30_digit_fifth_powers.rb
+++ solved/p30_digit_fifth_powers.rb
@@ -8,23 +8,16 @@
 # The sum of these numbers is 1634 + 8208 + 9474 = 19316.
 #
 # Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
-
+require_relative 'euler_utils'
 
 def can_be_written?(n)
-  save = n
-  digs = []
-  while n > 0 do
-    digs << n % 10
-    n -= digs[-1]
-    n /= 10
-  end
   sum = 0
-  digs.each{|i| sum += i**5}
-
-  save == sum
+  digits = get_digits(n)
+  digits.each{|i| sum += i**5}
+  n==sum
 end
 
-max = (9**5) * 4 #Maybe??
+max = (9**5) * 4
 sum = 0
 max.times do |i|
   next if i < 2
